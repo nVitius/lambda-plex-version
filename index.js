9b25c5c466f2c51688b19a6e63981c2c971d0556
nVitius <mauriciow95@gmail.com>
2015-09-14 15:30:15 -0700

    Call app.js

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 console.log('Loading function');
 
-//var app = require('lib/app.js');
-//exports.check = app.check;
-exports.handler = function (event, context) {
-    var http = require('http');
-    var AWS = require("aws-sdk");
+exports.handler = function check(event, context) {
+    var app = require('./lib/app');
 
-    AWS.config.update({region: 'us-west-2'});
-
-    var dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
-    var params = {
-        TableName: "plex_versions",
-        IndexName: "plex_pass-insert_timestamp-index",
-        KeyConditionExpression: "plex_pass = :pass",
-        ExpressionAttributeValues: {
-            ":pass": {"S": event.pass}
-        },
-        ProjectionExpression: "version, insert_timestamp",
-        ScanIndexForward: false,
-        Limit: 1
+    var config = {
+        callback: context.done,
+        pass: event.pass,
+        credentials: {
+            user: "test",
+            pass: "test"
+        }
     };
-    dynamo.query(params, versionTtl);
+
+    app(config)
 };
-
-function versionTtl(err, response) {
-    console.log(response.Items[0].insert_timestamp.N);
-    if (err) {
-        return;
-    }
-
-    var now = (new Date()).getTime() / 1000 | 0;
-
-    if ((now - response.Items[0].insert_timestamp.N) <= 900) {
-    }
-}
